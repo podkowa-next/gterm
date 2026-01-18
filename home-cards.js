838d5d9ab631e0e6984453a75263050400310cc4
podkowa-next <podkowa@gmail.com>
2026-01-18 18:00:19 +0100

    Update home-cards.js
    Updated duration and stagger parameters.

--- home-cards.js
+++ home-cards.js
@@ -16,7 +16,7 @@
         scale: 1,
         duration: 1,
         ease: "expo.out",
-        delay: 1.5, // Reduced delay (3.5s might be too long for static content)
-        stagger: 0.25
+        delay: 1.25, // Reduced delay (3.5s might be too long for static content)
+        stagger: 0.35
     });
 })();
